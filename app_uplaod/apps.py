caf9e8461f5dd62e1a4da7cee0585240e08dc416
Madhusudhanan Jeyaram <madhusudhanan.jayaram@gmail.com>
2025-12-10 06:09:45 -0600

    Remove unused static files, CSS styles, and Django project settings to clean up the repository.

--- app_uplaod/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppUplaodConfig(AppConfig):
-    name = 'app_uplaod'
--- app_uplaod/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-class WhatsAppForm(forms.ModelForm):
-
-    class Meta():
-        model = WhatsApp
-        fields = ['upload_file']
